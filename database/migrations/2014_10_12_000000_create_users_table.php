daf3daa8a873fde3ed66d2ad2484b03d039c1148
Kevin <kevin.silva@nestdigital.com.br>
2023-05-12 16:23:55 -0300

    MediaLibrary e permissions

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,27 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id();            
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password');            
+            $table->string('document_cpf', 20)->nullable();
+            $table->string('document_cnpj', 20)->nullable();            
+            $table->string('phone', 25)->nullable();
+            $table->string('phone_mobile', 25)->nullable();
+            $table->string('phone_whatsapp', 25)->nullable();
+            $table->string('state', 2)->nullable();
+            $table->string('city')->nullable();
+            $table->string('neighborhood')->nullable();
+            $table->string('street')->nullable();
+            $table->string('number')->nullable();
+            $table->string('zipcode', 10)->nullable();
+            $table->string('complement')->nullable();            
+            $table->foreignId('created_by')->nullable()->constrained('users');            
+            $table->boolean('archived')->default(false);
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
