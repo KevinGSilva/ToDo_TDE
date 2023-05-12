1afba7b5647970e1849e23964271885567f0ee29
Kevin <kevin.silva@nestdigital.com.br>
2023-05-12 16:28:09 -0300

    Redirecionamento para o dashboard

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
 
-Route::get('/dashboard', [\App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
+Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
+Route::get('/tables', [\App\Http\Controllers\HomeController::class, 'index'])->name('tables');
